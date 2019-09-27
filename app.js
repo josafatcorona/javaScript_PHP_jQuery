111cc965a7975c7011d55c61eb9f1e3a69f6ce17
JosafatCorona <josafatcoronac@gmail.com>
2019-09-27 09:02:16 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(function(){
+	console.log('jQuery is Working')
+	$('#search').keyup(function(e){
+		let search=$('#search').val();
+		$.ajax({
+			url: 'task-search.php',
+			type: 'POST',
+			data: { search },
+			success: function(response){
+				console.log(response)
+			}
+		})
+	})
+});
---
+++ database.php
@@ -0,0 +1,13 @@
+<?php 
+	$connection = mysqli_connect(
+		'localhost',
+		'root',
+		'',
+		'tasks-app'
+	);
+	if($connection){
+		echo "Database is connected";
+	}else{
+		echo "Not connected";
+	}
+?>
